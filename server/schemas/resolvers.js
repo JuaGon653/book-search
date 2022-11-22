659a74040541644846776248402ee1a1d7985332
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-22 14:52:20 -0600

    implemented single user query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,17 @@ const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id);
 
+                return user;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    }
 };
 
 module.exports = resolvers;
